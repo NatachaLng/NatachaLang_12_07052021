23a55eeed3073eccc9a4130d49113d1a7dc61284
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2021-05-31 08:53:15 +0100

    Update fetch.js

--- src/service/fetch.js
+++ src/service/fetch.js
@@ -9,7 +9,6 @@ const fetchData =  async (id, option) => {
     let url = option ? `http://localhost:3001/user/${id}/${option}` : `http://localhost:3001/user/${id}`
     let response = await fetch(url)
     let data = await response.json()
-    console.log(data)
     return data.data
 }
 
